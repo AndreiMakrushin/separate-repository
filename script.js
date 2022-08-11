67682fd2fd3cd6e66bc2f384da331704b62081c7
Andrei Makrushinemail <andrei18makrushin@gmail.com>
2022-08-12 00:54:28 +0500

    переделал

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
+'use strict'
 const eBtn = document.getElementById('e_btn');
-eBtn.style="display: none;"
+eBtn.style = "display: none;"
 //------------------------------------------------------------
 
 const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 const square = document.getElementById('square');
-
-btn.addEventListener('click', function(){
+const range = document.getElementById('range');
+let span = document.getElementById('range-span');
+const circle = document.getElementById('circle');
+span.textContent = '50%';
+btn.addEventListener('click', function () {
     square.style.background = `${text.value}`;
     text.value = ''
 });
+
+const rangeSpan = function (val) {
+    span.textContent = val + "%";
+    circle.style.height = `${val}%`;
+    circle.style.width = `${val}%`;
+}
 //-------------------------------------------------------------
-
-const range = document.getElementById('range');
-const span = document.getElementById('range-span');
-const circle = document.getElementById('circle');
-
-range.addEventListener ('input', e => {
-    span.textContent = e.target.value + "%";
-    circle.style.height = `${span.textContent}`;
-    circle.style.width = `${span.textContent}`;
-    
+rangeSpan(50);
+range.addEventListener('input', function (e) {
+    rangeSpan(e.target.value)
 });
+
 
