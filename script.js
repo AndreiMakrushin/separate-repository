7f9c6e43e067fa4014bc8b0f50e387fd045b554f
Andrei Makrushinemail <andrei18makrushin@gmail.com>
2022-08-11 20:38:39 +0500

    сделал домашку

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ eBtn.style="display: none;"
 const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 const square = document.getElementById('square');
+
 btn.addEventListener('click', function(){
     square.style.background = `${text.value}`;
     text.value = ''
@@ -14,6 +15,7 @@ btn.addEventListener('click', function(){
 const range = document.getElementById('range');
 const span = document.getElementById('range-span');
 const circle = document.getElementById('circle');
+
 range.addEventListener ('input', e => {
     span.textContent = e.target.value + "%";
     circle.style.height = `${span.textContent}`;
